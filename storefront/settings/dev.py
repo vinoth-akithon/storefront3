6e3b297356732903699b2a37465966d4659d03ec
Vinoth Kumar <vinoth.akithon@gmail.com>
2023-12-21 00:22:43 +0530

    ...

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -8,8 +8,7 @@ SECRET_KEY = 'django-insecure-y+=vy3%+!i@0%^j*8^mj1ss9o=%w9tb5(s^vq4ikt3b)+^-yup
 
 DATABASES = {
     'default': dj_database_url.config(
-        # default="mysql://root:Success.2023@localhost:3306/storefront3"
-        default="postgres://vinoth:P23ZkpKpJMCsR507tIPZEzS3Nl60qbTR@dpg-cm1f83a1hbls73ai2k2g-a.singapore-postgres.render.com/storefront_q28e"
+        default="mysql://root:Success.2023@localhost:3306/storefront3"
     )
 }
 
