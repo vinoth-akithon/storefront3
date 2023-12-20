f2d31f30a42d4746089760648a42e258d2476ffc
Vinoth Kumar <vinoth.akithon@gmail.com>
2023-12-20 23:30:26 +0530

    adding static files

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -108,8 +108,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 
 MEDIA_URL = "media/"
 
